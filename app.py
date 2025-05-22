ceb30bc140442415a2bba02f19c8a3e70d54a712
Jackson Perry <jackson.perry@outlook.com>
2025-05-21 22:08:58 -0400

    update app to use env or github actions

--- app.py
+++ app.py
@@ -3,9 +3,17 @@ import requests
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+# Only load .env file when running locally (i.e., not on GitHub Actions)
+if os.getenv('FLASK_ENV') != 'production':  # Adjust this condition based on your setup
+    load_dotenv()
 
-app_id = os.environ["app_id"]
+# Retrieve the `APP_ID` from environment variables (either GitHub Secrets or .env)
+app_id = os.getenv('app_id')  # This will work with both GitHub Secrets or .env
+
+if not app_id:
+    raise ValueError("Aapp_idis required but not set in environment variables.")
+
+#app_id = os.environ["app_id"]
 app = Flask(__name__)
 
 baseurl = f"https://openexchangerates.org/api/latest.json?app_id={app_id}&symbols=GBP,EUR,AED,CAD"
