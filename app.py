3d80a2cb7d2b70d21030aab26bd3a5b6f28266a9
Jackson Perry <jackson.perry@outlook.com>
2025-05-21 21:57:23 -0400

    clead up app working locally

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 import requests
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app_id = os.environ["app_id"]
 app = Flask(__name__)
 
-baseurl = f"https://openexchangerates.org/api/latest.json?app_id=[[app:{app_id}]]&symbols=GBP,EUR,AED,CAD"
+baseurl = f"https://openexchangerates.org/api/latest.json?app_id={app_id}&symbols=GBP,EUR,AED,CAD"
 
 response = requests.get(baseurl)
 
@@ -18,4 +21,7 @@ else:
 
 @app.route("/")
 def home():
-    return data
+    return str(data)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
